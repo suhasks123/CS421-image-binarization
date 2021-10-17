1bb0cca507dbfc62f28df4aae84796fd45f7fb4a
rohithsaji <rohithsaji101@gmail.com>
2021-10-17 12:53:05 +0530

    kmeans and otsu error fixed and PSNR added

--- src/Otsu and K-means.m
+++ src/Otsu and K-means.m
@@ -1,23 +1,26 @@
 clear all;
-for i=1:10
-     k=imread(strcat('og/',string(i),'.bmp'));
-     kmeansresult=Kmeans(k);
-     otsuresult=Kmeans(k);
-     kg=imread(strcat('og/',string(i),'.bmp'));
-     kg=rgb2gray(kg);
-     kg=im2double(kg);
-     kmeanes=immse(kmeansresult,kg);
-     ostures=immse(otsuresult,kg);
-     imwrite(kmeansresult,strcat('kmeans_results/',string(i),'.bmp'));
-     imwrite(otsuresult,strcat('otsu_results/',string(i),'.bmp'));
-     temp=[ostures,kmeanes];
-     disp(temp);
-         
-end 
- 
+% Denoise();
+findPSNR();
 
+function Denoise()
+    for i=1:10
+         k=imread(strcat('og/',string(i),'.bmp'));
+         kmeansresult=Kmeans(k);
+         otsuresult=findotsu(k);
+         otsuresult=double(otsuresult);
+         kg=imread(strcat('ground/',string(i),'.bmp'));
+         kg=rgb2gray(kg);
+         kg=im2double(kg);
+         kmeanes=immse(kmeansresult,kg);
+         ostures=immse(otsuresult,kg);
+         imwrite(kmeansresult,strcat('kmeans_results/',string(i),'.bmp'));
+         imwrite(otsuresult,strcat('otsu_results/',string(i),'.bmp'));
+         temp=[ostures,kmeanes];
+         disp(temp);
 
-function result=utsu(img)
+    end 
+end
+function result=findotsu(img)
     T=graythresh(img);
     result=im2bw(img,T);
 end 
@@ -29,3 +32,24 @@ function result=Kmeans(img)
      J=1-abs(J);
      result=J;
 end 
+function findPSNR()
+    for i=1:10
+        kg=imread(strcat('ground/',string(i),'.bmp'));
+        kg=rgb2gray(kg);
+        kg=im2double(kg);
+        ostures=imread(strcat('otsu_results/',string(i),'.bmp'));
+        ostures=im2double(ostures);
+        kmeansres=imread(strcat('kmeans_results/',string(i),'.bmp'));
+        kmeansres=im2double(kmeansres);
+        psnrotsu=psnr(ostures,kg);       
+        psnrkmeans=psnr(kmeansres,kg);
+        temp=[psnrotsu,psnrkmeans];
+        disp(temp);
+    end 
+end 
+
+
+
+
+
+
